12510e047f1b31792c5a3fc717e5b688e50fb40e
Essien Saviour <IbibioDeveloper@Wisdomanthonis-MacBook-Pro-2.local>
2019-01-09 18:23:20 +0100

    Updated JS

--- js/app.js
+++ js/app.js
@@ -1,24 +1,3 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("user-menu-id").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.user')) {
-      var dropdowns = document.getElementsByClassName("user-menu");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
-
-
 function notifFunction() {
     document.getElementById("notif-menu-id").classList.toggle("show");
   }
